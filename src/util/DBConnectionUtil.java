e794a78fb72435e7c701d9b66f965a852d46f4fd
Your Name <vinishasv97@gmail.com>
2025-05-08 13:20:01 +0530

    test commit

--- src/util/DBConnectionUtil.java
+++ src/util/DBConnectionUtil.java
@@ -1,5 +1,17 @@
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+ 
 public class DBConnectionUtil {
-
+	
+	public static final String URL="jdbc:/postgresql://localhost:5432//ContactMangementSystem ";
+	public static final String USERNAME="postgres";
+    public static final String PASSWORD="vinisha@97";
+    
+    public static Connection getConnection() throws SQLException{
+    	 
+    	return DriverManager.getConnection(URL,USERNAME,PASSWORD);
+    }
 }
